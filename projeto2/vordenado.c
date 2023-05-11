6ac1fb37ea6a4ef38c3c835cdfde81a0f0d88d0b
Rodrigo Santos <71847942+Saints12@users.noreply.github.com>
2023-05-10 20:32:41 -0400

    Update and rename lordenado.c to vordenado.c

---
+++ projeto2/vordenado.c
@@ -0,0 +1,121 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "vordenado.h"
+
+typedef struct elem_lse t_elemento_lse;
+struct elem_lse{
+    void* cargautil;
+    struct elem_lse* prox;
+};
+
+
+t_elemento_lse* criar_elemento_lse(void* cargautil){
+    t_elemento_lse* novo = malloc(sizeof(t_elemento_lse));
+    novo->cargautil = cargautil;
+    novo->prox = NULL;
+
+    return novo;
+}
+
+struct lse{
+    t_elemento_lse* inicio; 
+    int tamanho;
+    t_imprimir_lse impressora; 
+    t_comparar_lse comparar;
+    int nro_cmp_ins;
+    int nro_cmp_pri;
+    int nro_cmp_rem;
+};
+
+t_lse* criar_lse(t_imprimir_lse impressora, t_comparar_lse comparar){
+     t_lse* nova;
+     
+    nova  = malloc(sizeof(t_lse));
+    nova->inicio = NULL;
+    nova->tamanho = 0;
+    nova->impressora = impressora;
+    nova->comparar = comparar;
+    nova -> nro_cmp_ins = 0;
+    nova -> nro_cmp_pri = 0;
+    nova -> nro_cmp_rem = 0;
+
+    return nova;
+}
+
+void inserir_lse(t_lse* lse, void* nova_carga){
+    t_elemento_lse* cam = lse->inicio;
+    t_elemento_lse* ant=NULL;
+
+    lse->tamanho++;
+    t_elemento_lse* novo = criar_elemento_lseO(nova_carga);
+    if (lse->inicio == NULL){
+       lse->inicio = novo;
+    }else{
+        while( (cam!=NULL) && (lse->comparar(cam->cargautil, nova_carga)<0)){
+            ant = cam;
+            cam = cam->prox;
+            lse -> nro_cmp_ins++;
+        }
+        
+        if(cam == lse->inicio){
+            novo->prox = cam;
+            lse->inicio = novo;
+        }else{
+            ant->prox = novo;
+            novo->prox = cam;
+            
+        }
+    }
+}
+
+
+void* remover_lse(t_lse* lse, void* chave){
+    t_elemento_lse* cam = lse->inicio;
+    t_elemento_lse* ant = NULL;
+    
+    while( (cam!=NULL) && (lse->comparar(cam->cargautil , chave)!=0)){
+        ant = cam;
+        cam = cam->prox;
+        lse -> nro_cmp_rem++;
+    }
+
+    void* carga = NULL;
+    if (cam != NULL){
+        carga = cam->cargautil;
+        if (cam == lse->inicio){ 
+            lse->inicio = cam->prox;
+        }else{
+            ant->prox = cam->prox;    
+        }
+        free(cam);
+        lse->tamanho--;
+    }
+    return carga; 
+
+}
+
+void* menor_vordenado(t_lse* lse){
+    t_elemento_lse* cam = lse -> inicio; 
+    return cam -> cargautil;
+}
+
+void* maior_vordenado(t_lse* lse){
+    void* carga = NULL;
+    
+    if(lse->inicio == NULL){
+        return carga;
+    }else{
+        t_elemento_lse* cam = lse->inicio;
+        while(cam->prox!=NULL){
+            cam=cam->prox;
+        }
+        carga = cam -> cargautil;
+        return carga;
+    }
+}
+
+void estatistica_vordenado(t_lse* lse){
+    printf("No. de comparacoes nas insercoes: %d\nNo. de comparacoes nas remocoes: %d\nNo. de comparacoes no acesso ao primeiro elemento: %d\n", lse->nro_cmp_ins,lse->nro_cmp_rem,lse->nro_cmp_pri);
+}
+
